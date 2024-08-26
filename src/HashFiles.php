022233b033ec27152aa24e0f6af61629e5122d68
devuri <devuri@users.noreply.github.com>
2024-08-25 20:04:34 -0500

    fix: only hash images and PDFs

--- src/HashFiles.php
+++ src/HashFiles.php
@@ -12,25 +12,56 @@ namespace FileNameHasher;
 
 class HashFiles
 {
-    public function __construct()
+    private $hashed_files = [];
+    private $allowed_extensions;
+
+    public function __construct( array $allowed_extensions )
     {
-        add_filter( 'wp_handle_upload_prefilter', [ $this, 'filename_to_hash' ] );
+        $this->allowed_extensions = $allowed_extensions;
     }
 
-    public function filename_to_hash( $file )
+    public function add_prefilter(): void
     {
-        $file_info   = pathinfo( $file['name'] );
-        $extension   = $file_info['extension'];
+        add_filter( 'wp_handle_upload_prefilter', [ $this, 'upload_to_hash' ] );
+    }
 
-		if ( $extension === 'zip' ) {
-			return $file;
-		}
+    public function upload_to_hash( $file )
+    {
+        if ( ! isset( $file['name'] ) ) {
+            return $file;
+        }
 
-        $unique_id   = bin2hex( random_bytes( 32 ) );
-        $hashed_name = hash( 'sha256', $unique_id );
-        $new_file_name = $hashed_name . '.' . $extension;
-        $file['name']  = $new_file_name;
+        $file_info = pathinfo( $file['name'] );
+
+        if ( ! isset( $file_info['extension'] ) ) {
+            return $file;
+        }
+
+        $extension = strtolower( $file_info['extension'] );
+
+        // Check if the extension is in the allowed list
+        if ( \in_array( $extension, $this->allowed_extensions, true ) ) {
+            $unique_id     = bin2hex( random_bytes( 32 ) );
+            $hashed_name   = hash( 'sha256', $unique_id );
+            $new_file_name = $hashed_name . '.' . $extension;
+            $file['name']  = $new_file_name;
+
+            // Check if the filename is already hashed.
+            if ( ! $this->is_file_hashed( $new_file_name ) ) {
+                $this->hashed_files[] = $new_file_name;
+            }
+        }
 
         return $file;
+    }
+
+    public function get_allowed_extensions()
+    {
+        return $this->allowed_extensions;
+    }
+
+    protected function is_file_hashed( $filename )
+    {
+        return \in_array( $filename, $this->hashed_files, true );
     }
 }
