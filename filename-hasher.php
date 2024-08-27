eaa130bc8c0a2dc0cb072e2bdf3f16104c5aeb0a
devuri <devuri@users.noreply.github.com>
2024-08-27 01:08:02 -0500

    feat: adds `add_uniqid` with `uniqid()`

--- filename-hasher.php
+++ filename-hasher.php
@@ -24,18 +24,21 @@ if ( ! \defined( 'ABSPATH' ) ) {
 \define( 'ALLOWED_EXTENSIONS_OPTION_ID', 'wpfnh_allowed_extensions' );
 \define( 'KEEP_ORIGINAL_PREFIX_OPTION_ID', 'wpfnh_keep_original_prefix' );
 \define( 'CUSTOM_PREFIX_OPTION_ID', 'wpfnh_custom_prefix' );
+\define( 'ADD_UNIQID_OPTION_ID', 'wpfnh_add_uniqid' );
 
 require_once plugin_dir_path( __FILE__ ) . 'src/HashFiles.php';
 
 $allowed_extensions   = explode( ',', get_option( ALLOWED_EXTENSIONS_OPTION_ID, 'jpg,jpeg,png,gif,bmp,pdf' ) );
 $keep_original_prefix = get_option( KEEP_ORIGINAL_PREFIX_OPTION_ID, '0' );
 $custom_prefix        = get_option( CUSTOM_PREFIX_OPTION_ID, '' );
+$add_uniqid = get_option( ADD_UNIQID_OPTION_ID, '1' );
 
 // maybe later allow the user to change allowed files.
 $file_hasher = new FileNameHasher\HashFiles(
     $allowed_extensions,
     (int) $keep_original_prefix,
-    $custom_prefix
+    $custom_prefix,
+	(int) $add_uniqid
 );
 
 $file_hasher->add_prefilter();
@@ -47,5 +50,6 @@ register_activation_hook(
 		delete_option( ALLOWED_EXTENSIONS_OPTION_ID );
 		delete_option( KEEP_ORIGINAL_PREFIX_OPTION_ID );
 		delete_option( CUSTOM_PREFIX_OPTION_ID );
+		delete_option( ADD_UNIQID_OPTION_ID );
 	}
 );
